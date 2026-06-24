0a4218a8e827a727d390be9b60855227a857b8de
GDivyaDharshini <divimano2007@gmail.com>
2026-06-24 17:49:42 +0530

    Added emergency alert and caregiver voice system

--- gesture_emergency/emergency.py
+++ gesture_emergency/emergency.py
@@ -1,3 +1,22 @@
-def send_emergency_alert():
-    print("🚨 EMERGENCY ALERT SENT")
-    print("Notifying caregiver...")
+import pyttsx3
+import winsound
+import time
+
+engine = pyttsx3.init()
+
+def trigger_emergency():
+
+    print("🚨 EMERGENCY ALERT ACTIVATED 🚨")
+
+    for i in range(3):
+        winsound.Beep(1000, 500)
+        time.sleep(0.2)
+
+    engine.say(
+        "Emergency. Caregiver assistance required immediately."
+    )
+
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    trigger_emergency()
